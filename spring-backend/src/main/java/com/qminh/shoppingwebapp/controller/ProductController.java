1c19492b6a4a36ee64a1bf8de51694b44bcd19af
CircleQMinh <quocminh.vutran3105@gmail.com>
2021-07-03 16:37:36 +0700

    03-07-2021

--- spring-backend/src/main/java/com/qminh/shoppingwebapp/controller/ProductController.java
+++ spring-backend/src/main/java/com/qminh/shoppingwebapp/controller/ProductController.java
@@ -1,11 +1,9 @@
 package com.qminh.shoppingwebapp.controller;
-import com.qminh.shoppingwebapp.exception.ResourceNotFoundException;
 import com.qminh.shoppingwebapp.model.Product;
-import com.qminh.shoppingwebapp.model.User;
 import com.qminh.shoppingwebapp.repo.ProductRepository;
-import com.qminh.shoppingwebapp.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +19,30 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
-    @GetMapping("/Products")
+    @GetMapping("/Products/all")
     public List<Product> getAllProduct() {
         return productRepository.findAll();
     }
+
+    @GetMapping("/Products")
+    Page<Product> getProductPage(@RequestParam(defaultValue = "1") int pageNumber,
+                                 @RequestParam(defaultValue = "8") int pageSize,
+                                 @RequestParam(defaultValue = "") String category){
+        Page<Product> page;
+        if(category.equals("all")){
+            page = (Page<Product>) productRepository.findAll(PageRequest.of(pageNumber-1,pageSize));
+        }
+        else{
+            page=productRepository.findByCategory(category,PageRequest.of(pageNumber-1,pageSize));
+        }
+        return page;
+    }
+    @GetMapping("/Products/search")
+    Page<Product> getSearchResult(@RequestParam(defaultValue = "") String keyword){
+
+        return productRepository.findByNameContaining(keyword,PageRequest.of(0,1000));
+    }
+
+
+
 }
